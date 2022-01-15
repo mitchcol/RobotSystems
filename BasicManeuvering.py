5ef664f5ff8c912bcc38b4e1f71d3e302fb5b18c
coamitch <coamitch@gmail.com>
2022-01-14 23:59:18 -0800

    made minor edits to ui. testing forward and backward commands.

--- BasicManeuvering.py
+++ BasicManeuvering.py
@@ -1,36 +1,89 @@
 import picarx_improved as pi
+import time
 
 # note! all these functions take a list as their argument. any argument checking is done
 # inside the function body. Remember, the first element in the list is the function name.
 
-def forward(args: list):
-	# forward(speed, angle)
-	return
+# using class structure here since it'll be easier than passing the picar object and checking
+# type each use.
+class BasicManeuvering():
+	# constructor
+	def __init__(self):
+		# creating picar object
+		self.px = pi.Picarx()
 
-def backward(args: list):
-	# backward(speed, angle)
-	return
+		# loading dictionary with callable functions. acts like a switch
+		self.switch = dict()
+		self.switch['forward'] = self.forward
+		self.switch['backward'] = self.backward
+		self.switch['parallelPark'] = self.parallelPark
+		self.switch['kTurn'] = self.kTurn
 
-def parallelPark(args: list):
-	# parallelPark(speed, dir)
-	return
+	# member methods
+	def checkArgs(self, args: list, numArgs):
+		# just checks that the list has the correct number of arguments
+		# remember that the function is still in the list of passed arguments
+		if len(args) == (numArgs+1):
+			return True
+		else:
+			return False
 
-def kTurn(args: list):
-	# kTurn(speed, initAng, dir)
-	return
+	def forward(self, args: list):
+		# forward(speed, angle, time)
 
-# loading dictionary with callable functions. acts like a switch
-switch = dict()
-switch['forward'] = forward
-switch['backward'] = backward
-switch['parallelPark'] = parallelPark
-switch['kTurn'] = kTurn
+		# checking arguments
+		numArgs = 3
+		if not self.checkArgs(args, numArgs):
+			raise UserWarning
+
+		speed = args[1]
+		angle = args[2]
+		driveTime = args[3]
+
+		# setting angle
+		self.px.set_dir_servo_angle(angle)
+
+		# moving forward
+		self.px.forward(speed)
+		time.sleep(driveTime)
+		self.px.stop()
+
+	def backward(self, args: list):
+		# backward(speed, angle, time)
+
+		# checking arguments
+		numArgs = 3
+		if not self.checkArgs(args, numArgs):
+			raise UserWarning
+
+		speed = args[1]
+		angle = args[2]
+		driveTime = args[3]
+
+		# setting angle
+		self.px.set_dir_servo_angle(angle)
+
+		# moving backward
+		self.px.backward(speed)
+		time.sleep(driveTime)
+		self.px.stop()
+
+	def parallelPark(self, args: list):
+		# parallelPark(speed, dir)
+		return
+
+	def kTurn(self, args: list):
+		# kTurn(speed, initAng, dir)
+		return
 
 if __name__ == '__main__':
+	# creating basic maneuvering object
+	bmObj = BasicManeuvering()
+
 	# printing help menu
 	print('Please enter one of the commands below (comma separated):')
-	print('\t- forward(speed, angle)')
-	print('\t- backward(speed, angle)')
+	print('\t- forward(speed, angle, time)')
+	print('\t- backward(speed, angle, time)')
 	print('\t- parallelPark(speed, angle)')
 	print('\t- kTurn(speed, initAng, dir)')
 	print('\t- exit <--- terminates the program')
@@ -51,10 +104,12 @@ if __name__ == '__main__':
 			done = True
 		else:
 			try:
-				switch[inputList[0]](inputList)
-				print(inputList)
+				bmObj.switch[inputList[0]](inputList)
 			except KeyError:
 				# function doesn't exist
-				print('\tPlease verify that your command is in the list.')
+				print('\tCommand not found. Please verify with the list above.')
+				print('\tUsage: function,arg1,arg2,arg3,...')
+			except UserWarning:
+				print('\tToo few or too many arguments. Please verify with the list above.')
 				print('\tUsage: function,arg1,arg2,arg3,...')
 
