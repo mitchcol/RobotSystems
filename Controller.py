9e9794023860eac84dfc6747eab5e7aea09cd24c
coamitch <coamitch@gmail.com>
2022-01-25 18:44:24 -0800

    Implemented main controller function. testing required.

--- Controller.py
+++ Controller.py
@@ -1,14 +1,20 @@
-
+import picarx_improved as pci
 
 class Controller():
 	# constructor
-	def __init__(self, offset):
-		self._offset = offset
+	def __init__(self, scale=10):
+		self._scale = scale
 
 	# getters/setters
 	@property
-	def offset(self):
-		return self._offset
+	def scale(self):
+		return self._scale
 
 	# member methods
-	def control(self):
+	def control(self, px: pci.Picarx, position: float):
+		# method to set the steering servo based on the input offset
+		steeringAngle = -1 * self._scale * position
+		px.set_dir_servo_angle(steeringAngle)
+		return steeringAngle
+
+
