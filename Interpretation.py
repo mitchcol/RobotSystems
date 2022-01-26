65c501122920a465603e03961702e44002fcc400
coamitch <coamitch@gmail.com>
2022-01-25 16:50:48 -0800

    pushed with script. fine tuning code...

--- Interpretation.py
+++ Interpretation.py
@@ -7,9 +7,21 @@ class Polarity(Enum):
 
 class Interpretation():
 	# constructor
-	def __init__(self, sensitivity=1, polarity=Polarity.DARK):
-		self._sensitivity = sensitivity
-		self._polarity = polarity
+	def __init__(self, sensitivity=0.5, polarity=Polarity.DARK):
+		# making sure polarity is of polarity type
+		if isinstance(polarity, Polarity):
+			self._polarity = polarity
+		else:
+			# if it isn't then we set the normal default
+			self._polarity = Polarity.DARK
+
+		# making sure the sensitivity is between 0 and 1.
+		if sensitivity > 1:
+			self._sensitivity = 1
+		elif sensitivity < 0:
+			self._sensitivity = 0
+		else:
+			self._sensitivity = sensitivity
 
 	# getters/setters
 	@property
@@ -27,54 +39,42 @@ class Interpretation():
 		# clamping the values to the min and max bounds provided in the documentation
 		s0, s1, s2 = self.clampADCValues(adcValueList)
 
-		# normalizing the current values
-		s0Norm = self.normalizeValue(s0, 900, 1500)
-		s1Norm = self.normalizeValue(s1, 900, 1500)
-		s2Norm = self.normalizeValue(s2, 900, 1500)
+		# bit of a slick conceptual equation here that Abhinav and I discussed. We subtract the two outer values to
+		# determine if we are pointed to the left or right then, we divide by the center sensor to normalize the
+		# value between -1 and 1. The higher the center value the smaller position will be, the smaller the
+		# center value, the higher the turn will be
+		pos = ((s2 - s0) / s1) * self._sensitivity
 
-		# inverting the norm if
-		if self._polarity.LIGHT:
-			s0Norm = 1 - s0Norm
-			s1Norm = 1 - s1Norm
-			s2Norm = 1 - s2Norm
-
-		# bit of a slick conceptual equation here that Abhinav told me about. After we normalize the values
-		# between 0 and 1, we subtract the two outer values to determine if we are pointed to the left or right
-		# then, we divide by the center sensor to normalize the value between -1 and 1. The higher the center value
-		# the smaller position will be, the smaller the center value, the higher the turn will be
-		try:
-			pos = (s2Norm - s0Norm) / s1Norm
-		except ZeroDivisionError:
-			# if the s1 value is 0, then the middle sensor isn't on the line at all
-			if s2Norm >= s0Norm:
-				pos = s2Norm
-			else:
-				pos = s0Norm
+		# checking if we have light polarity.
+		if self._polarity == Polarity.LIGHT:
+			# in this case, we should be able to just flip the sign
+			pos = -1 * pos
 
 		return pos
 
 	def clampADCValues(self, adcValueList: list):
 		# clamps the adc values in the list to values between 900 (black) and 1500 (white)
-		minValue = 900
+		# reduced the min value so we dont have issues normalizing the values later.
+		minValue = 750
 		maxValue = 1500
 
-		if adcValueList[0] < 900:
+		if adcValueList[0] < minValue:
 			s0 = minValue
-		elif adcValueList[0] > 1500:
+		elif adcValueList[0] > maxValue:
 			s0 = maxValue
 		else:
 			s0 = adcValueList[0]
 
-		if adcValueList[1] < 900:
+		if adcValueList[1] < minValue:
 			s1 = minValue
-		elif adcValueList[1] > 1500:
+		elif adcValueList[1] > maxValue:
 			s1 = maxValue
 		else:
 			s1 = adcValueList[1]
 
-		if adcValueList[2] < 900:
+		if adcValueList[2] < minValue:
 			s2 = minValue
-		elif adcValueList[2] > 1500:
+		elif adcValueList[2] > maxValue:
 			s2 = maxValue
 		else:
 			s2 = adcValueList[2]
